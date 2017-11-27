fdb294849409b04ad79a4c5934318a35ae34d76e
Ali <ali.nikseresht21@gmail.com>
2017-11-27 18:12:19 -0500

    Added arduino code to filestructure

---
+++ arduino/ecgAdc.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(19200);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+}
+
+void loop() {
+  if ((digitalRead(10) == 1) || (digitalRead(11) == 1)){
+    Serial.println('!');
+  }else{
+    Serial.println(analogRead(A0));
+  }
+  delay(16);
+}
